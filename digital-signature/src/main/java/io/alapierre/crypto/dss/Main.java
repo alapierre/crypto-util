be1a17ef6a8ce999ffde031fc7501537aca18fab
Adrian Lapierre <al@alapierre.io>
2022-01-03 19:06:42 +0100

    release prepare

---
+++ digital-signature/src/main/java/io/alapierre/crypto/dss/Main.java
@@ -0,0 +1,14 @@
+package io.alapierre.crypto.dss;
+
+import javax.naming.InvalidNameException;
+
+/**
+ * @author Adrian Lapierre {@literal al@alapierre.io}
+ * Copyrights by original author 2021.12.23
+ */
+public class Main {
+
+    public static void main(String[] args) throws InvalidNameException {
+        DssUtil.printCerts();
+    }
+}
