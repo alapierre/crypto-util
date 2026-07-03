df1fc6eda62f11c9d2c1987b25220afc299ba4e1
Adrian Lapierre <al@itrust.dev>
2026-07-03 14:20:08 +0200

    enhance `CardSigner` with improved constructor options and useSlotListIndex handling

--- digital-signature/digital-signature-xades/src/main/java/io/alapierre/crypto/dss/signer/CardSigner.java
+++ digital-signature/digital-signature-xades/src/main/java/io/alapierre/crypto/dss/signer/CardSigner.java
@@ -4,7 +4,6 @@ import eu.europa.esig.dss.token.PasswordInputCallback;
 import eu.europa.esig.dss.token.Pkcs11SignatureToken;
 import eu.europa.esig.dss.token.SignatureTokenConnection;
 import io.alapierre.crypto.misc.DllUtil;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import static io.alapierre.crypto.misc.DllUtil.resolveDllAbsolutePathAndFileName;
@@ -14,17 +13,38 @@ import static io.alapierre.crypto.misc.DllUtil.resolveDllAbsolutePathAndFileName
  * Copyrights by original author 2021.12.23
  */
 @Slf4j
-@RequiredArgsConstructor
 public class CardSigner extends AbstractSigner {
 
     private final String relativePathToDll;
     private final String dllName;
     private final int slot;
     private final PasswordInputCallback passwordInputCallback;
+    private final boolean useSlotIndex;
+
+    public CardSigner(String relativePathToDll, String dllName, int slot, PasswordInputCallback passwordInputCallback) {
+        this.relativePathToDll = relativePathToDll;
+        this.dllName = dllName;
+        this.slot = slot;
+        this.passwordInputCallback = passwordInputCallback;
+        this.useSlotIndex = false;
+    }
+
+    public CardSigner(String relativePathToDll, String dllName, int slot, boolean useSlotListIndex, PasswordInputCallback passwordInputCallback) {
+        this.relativePathToDll = relativePathToDll;
+        this.dllName = dllName;
+        this.slot = slot;
+        this.passwordInputCallback = passwordInputCallback;
+        this.useSlotIndex = useSlotListIndex;
+    }
 
     @Override
     protected SignatureTokenConnection prepareToken() {
         DllUtil.DllInfo dllInfo = resolveDllAbsolutePathAndFileName(relativePathToDll, dllName);
-        return new Pkcs11SignatureToken(dllInfo.getFullPath(), passwordInputCallback, slot);
+
+        if (!useSlotIndex) {
+            return new Pkcs11SignatureToken(dllInfo.getFullPath(), passwordInputCallback, slot);
+        } else {
+            return new Pkcs11SignatureToken(dllInfo.getFullPath(), passwordInputCallback, -1, slot, null);
+        }
     }
 }
